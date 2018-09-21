dee02a05fecedea28e0e60e80af395f50731b323
Maciej <maciej.czarnecki92@gmail.com>
2018-09-21 16:02:36 +0200

    Makes and Models DB update

---
+++ Migrations/20180920204447_UpdateMakes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Vega.Migrations
+{
+    public partial class UpdateMakes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Makes set Name = 'Ford' where Name = 'Make1'");
+            migrationBuilder.Sql("update Makes set Name = 'Renault' where Name = 'Make2'");
+            migrationBuilder.Sql("update Makes set Name = 'Skoda' where Name = 'Make3'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Makes set Name = 'Make1' where Name = 'Ford'");
+            migrationBuilder.Sql("update Makes set Name = 'Make2' where Name = 'Renault'");
+            migrationBuilder.Sql("update Makes set Name = 'Make3' where Name = 'Skoda'");
+        }
+    }
+}
---
+++ Migrations/20180920205008_UpdateModels.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Vega.Migrations
+{
+    public partial class UpdateModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Models set Name = 'Escort' where Name = 'Make1-ModelA'");
+            migrationBuilder.Sql("update Models set Name = 'S-Max' where Name = 'Make1-ModelB'");
+            migrationBuilder.Sql("update Models set Name = 'Galaxy' where Name = 'Make1-ModelC'");
+            migrationBuilder.Sql("update Models set Name = 'Megane' where Name = 'Make2-ModelA'");
+            migrationBuilder.Sql("update Models set Name = 'Laguna' where Name = 'Make2-ModelB'");
+            migrationBuilder.Sql("update Models set Name = 'Clio' where Name = 'Make2-ModelC'");
+            migrationBuilder.Sql("update Models set Name = 'Octavia' where Name = 'Make2-ModelA'");
+            migrationBuilder.Sql("update Models set Name = 'Superb' where Name = 'Make2-ModelB'");
+            migrationBuilder.Sql("update Models set Name = 'Kodiaq' where Name = 'Make2-ModelC'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Models set Name = 'Make1-ModelA' where Name = 'Escort'");
+            migrationBuilder.Sql("update Models set Name = 'Make1-ModelB' where Name = 'S-Max'");
+            migrationBuilder.Sql("update Models set Name = 'Make1-ModelC' where Name = 'Galaxy'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelA' where Name = 'Megane'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelB' where Name = 'Laguna'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelC' where Name = 'Clio'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelA' where Name = 'Octavia'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelB' where Name = 'Superb'");
+            migrationBuilder.Sql("update Models set Name = 'Make2-ModelC' where Name = 'Kodiaq'");
+        }
+    }
+}
